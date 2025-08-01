f0f6320091ad71850927213219dea708d9fb88a0
jmtjm45@gamil.com <jmtjm45@gmail.com>
2025-08-01 21:41:23 +0600

    Change the MessageRoutes.js for adding authMiddleware

--- Routes/MessageRoutes.js
+++ Routes/MessageRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getMessages, sendMessage } from '../Controllers/userController.js';
-import protectRoute from '../middleware/protectRoute.js';
+// Correct import statement:
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Get message history for a specific chat
-router.get('/:otherUserId', protectRoute, getMessages);
+router.get('/:otherUserId', protect, getMessages);
 
 // Send a new message
-router.post('/send/:otherUserId', protectRoute, sendMessage);
+router.post('/send/:otherUserId', protect, sendMessage);
 
 export default router;
